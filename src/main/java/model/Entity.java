1e4fd90baba5bd7e00d7997383489143e3026254
Miyuki2007 <25021779@uet.edu.vn>
2026-04-05 18:31:51 +0700

    add OOP base classes - Entity

---
+++ src/main/java/model/Entity.java
@@ -0,0 +1,26 @@
+package model;
+import java.io.Serializable;
+import java.util.UUID;
+public abstract class Entity implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private final String id;
+    protected Entity(){
+        this.id = UUID.randomUUID().toString();
+    }
+    protected Entity(String id){
+        this.id = id;
+    }
+    public abstract String getDisplayInfo();
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entity entity = (Entity) o;
+        return id.equals(entity.id);
+    }
+    @Override
+    public int hashCode(){
+        return id.hashCode();
+    }
+}
+
