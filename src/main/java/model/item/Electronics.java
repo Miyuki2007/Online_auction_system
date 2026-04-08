8cd65ce87fdf50ba9063d52efe6281bcd0c01f1c
phamthecong2412-art <phamthecong2412-github@gmail.com>
2026-04-08 22:39:55 +0700

    pthecong

---
+++ src/main/java/model/item/Electronics.java
@@ -0,0 +1,18 @@
+package model.item;
+
+
+public class Electronics extends Item {
+    private String brand;
+    public Electronics(String id, String name, String description, double price, String brand) {
+        super(id, name, description, price);
+        this.brand = brand;
+    }
+    @Override
+    public void displayDetails() {
+        System.out.println("Tên sản phẩm: " + getName() + " Loại vật phẩm: Đồ điện tử");
+        System.out.println("Id: " + getId());
+        System.out.println("Mô tả: " + getDescription() + " Nhãn hiệu: " + brand);
+        System.out.println("Giá khởi điểm: " + getStartingPrice());
+        System.out.println("Giá hiện tại: " + getCurrentPrice());
+    }
+}
---
+++ src/main/java/model/item/Item.java
@@ -0,0 +1,35 @@
+package model.item;
+
+public abstract class Item {
+    private String id;
+    private String name;
+    private String description;
+    private double startingPrice;
+    private double currentPrice;
+    public Item(String id, String name, String description, double startingPrice) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.startingPrice = startingPrice;
+        this.currentPrice = startingPrice;
+    }
+    public abstract void displayDetails();
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public double getStartingPrice() {
+        return startingPrice;
+    }
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+}
