5bd07365d1e84c05eaff6cd39ac72db34ea74f16
VuNgoc-hp <ngocsenpai06@gmail.com>
2026-04-16 11:28:15 +0700

    docs

--- src/main/java/model/auction/observer/AuctionObserver.java
+++ src/main/java/model/auction/observer/AuctionObserver.java
@@ -1,8 +1,10 @@
 package model.auction.observer;
 import model.auction.Auction;
-import model.BTransaction.BidTransaction;
-public interface AuctionObserver {
-        void onNewBid(Auction auction, BidTransaction bid);
+import model.auction.Auction.BidRecord;
+public class AuctionObserver {
+    public interface AuctionObserver{
+        void onNewBid(Auction auction, BidRecord bid);
         void onAuctionStateChanged(Auction auction);
         void onAuctionTimeExtended(Auction auction, long extensionSeconds);
+    }
 }
--- src/main/java/model/auction/observer/AuctionSubject.java
+++
@@ -1,8 +0,0 @@
-package model.auction.observer;
-import model.BTransaction.BidTransaction;
-public interface AuctionSubject{
-    void addObserver (AuctionObserver observer);
-    void removeObserver(AuctionObserver observer);
-    void notifyNewBid(BidTransaction bid);
-    void notifyStateChanged();
-}
