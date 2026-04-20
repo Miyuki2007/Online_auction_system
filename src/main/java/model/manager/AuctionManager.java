0e017fa04cd018514ac337642c9c1e61e151275d
Miyuki2007 <25021779@uet.edu.vn>
2026-04-20 10:06:42 +0700

    Commit JunitTest

--- src/main/java/model/manager/AuctionManager.java
+++ src/main/java/model/manager/AuctionManager.java
@@ -1,6 +1,8 @@
 package model.manager;
 
 import model.auction.Auction;
+import model.user.User;
+import model.auction.exception.AuthenticationException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,9 +12,11 @@ public class AuctionManager {
     private static volatile AuctionManager instance;
 
     private List<Auction> activeAuctions;
+    private List<User> registeredUsers;
 
     private AuctionManager() {
         activeAuctions = Collections.synchronizedList(new ArrayList<>());
+        registeredUsers = Collections.synchronizedList(new ArrayList<>());
     }
 
     public static AuctionManager getInstance() {
@@ -25,6 +29,28 @@ public class AuctionManager {
         }
         return instance;
     }
+    public void registerUser(User user){
+        registeredUsers.add(user);
+    }
+    public User getUserByUsername(String username){
+        for (User user : registeredUsers){
+            if (user.getUsername().equals(username)){
+                return user;
+            }
+        }
+        return null;
+    }
+    public User authenticateUser(String username,String password){
+        User user = getUserByUsername(username);
+        if (user==null){
+            throw new AuthenticationException("Tên đăng nhập không tồn tại.");
+
+        }
+        if (!user.authenticate(password)){
+            throw new AuthenticationException("Mật khẩu không đúng");
+        }
+        return user;
+    }
 
     public void startAuction(Auction auction) {
         activeAuctions.add(auction);
@@ -33,5 +59,8 @@ public class AuctionManager {
 
     public List<Auction> getActiveAuctions() {
         return activeAuctions;
+    }
+    public List<User> getRegisteredUsers(){
+        return Collections.unmodifiableList(registeredUsers);
     }
 }
