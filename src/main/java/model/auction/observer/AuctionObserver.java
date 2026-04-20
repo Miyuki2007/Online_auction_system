89b9293c3436435838b8475403903529fa1d7889
Miyuki2007 <25021779@uet.edu.vn>
2026-04-20 10:06:42 +0700

    Commit JunitTest

--- src/main/java/model/auction/observer/AuctionObserver.java
+++ src/main/java/model/auction/observer/AuctionObserver.java
@@ -1,10 +1,8 @@
 package model.auction.observer;
 import model.auction.Auction;
 import model.auction.BidTransaction;
-
 public interface AuctionObserver{
-    void onNewBid(Auction auction, BidTransaction bid);
-    void onAuctionStateChanged(Auction auction);
-    void onAuctionTimeExtended(Auction auction, long extensionSeconds);
-}
-
+        void onNewBid(Auction auction, BidTransaction bid);
+        void onAuctionStateChanged(Auction auction);
+        void onAuctionTimeExtended(Auction auction, long extensionSeconds);
+    }
