38ee19299f83b3982429e3fdf2a799a6fe44d8c9
VuNgoc-hp <ngocsenpai06@gmail.com>
2026-05-20 17:58:10 +0700

    feat (VuNgoc)

--- src/main/java/server/AuctionServer.java
+++ src/main/java/server/AuctionServer.java
@@ -19,10 +19,6 @@ public class AuctionServer {
     public void start(){
         // 1. Khởi tạo Instance của AuctionManager
         AuctionManager manager = AuctionManager.getInstance();
-
-        // 🌟 BƯỚC QUAN TRỌNG: Khôi phục lại các phiên đấu giá từ DB lên RAM ngay khi bật Server
-        manager.loadAuctionsFromDatabase();
-
         running = true;
         try(ServerSocket serverSocket = new ServerSocket(PORT)){
             System.out.println("Server đang chạy trên port: " + PORT);
